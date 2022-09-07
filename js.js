3264b57742f1c3e8dcb8d24e36254f6bf045cda0
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-07 10:03:47 -0500

    Archivos Base

---
+++ js.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    // 1. Escribir un programa que sume, reste, multiplique y divida dos números
+    let num = Number(prompt("Escribe un numero", 2))
+    let num2 = Number(prompt("Escribe un segundo numero", 2))
+
+    console.log(`1. Escribir un programa que sume, reste, multiplique y divida dos números`)
+    console.log(`El resultado: ${num} + ${num2} es ${num + num2}`)
+})
