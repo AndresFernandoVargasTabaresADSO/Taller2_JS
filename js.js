c760aac452af5fc10a565d372b66ec9075fe6bd6
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-21 07:35:02 -0500

    Ejercicio 3

--- js.js
+++ js.js
@@ -1,15 +1,18 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`22. Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
-    descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
-    de la compra.`)
+    console.log(`23.
+    • Generar el algoritmo que dé como resultado el tiempo estimado para el llenado de un
+    depósito.
+    • Q=V/t, siendo Q (caudal), V (volumen) y t (tiempo).
+    • Volumen = PI *(radio^2)* H (altura del depósito)
+    • El tiempo se encuentra en minutos.
+    • Normalmente se mide el volumen en litros y el tiempo en segundos.`)
 
-    let c1= Number(prompt(`Ingresa el valor de la compra 1: `))
-    let c2= Number(prompt(`Ingresa el valor de la compra 2: `))
-    let c3= Number(prompt(`Ingresa el valor de compra 3: `))
-    let c4= Number(prompt(`Ingresa el valor de la compra 4: `))
-    let c5= Number(prompt(`Ingresa el valor de la compra 5: `))
+    let diametro= Number(prompt(`Ingresa el diametro: `))
+    let altura= Number(prompt(`Ingresa la altura: `))
 
-    let solucion= (c1+c2)*0.5 + (c3+c4)*0.20 + c5
-
-    console.log(`El valor total es: ${solucion}`)
+    let di=  diametro/2
+    let volumen= Math.PI * (Math.pow(di,2)* altura)
+    let q= volumen/60
+    
+    console.log(`Tiempo estimado ${q}`)
 })
