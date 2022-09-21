8f8d61a6fb35f776acf4fc0b30ac9aef0cdba613
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-21 08:20:39 -0500

    Ejercicio 7

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg transporta ?`)
+    console.log(`27. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
+    2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
+    Lorena?`)
 
 
-    var caja=25
-    var peso=748
-    var suma=caja*peso
-
-    console.log(`Transporta: Kg ${suma}`)
+    let e1=(2/3)*300
+    let e2=(1/4)*300
+    let e3=(5/60)*300
+    console.log(`Lorena le corresponde el 5/60 ${e3}`)
 })
