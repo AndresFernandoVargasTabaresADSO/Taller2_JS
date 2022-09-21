e86f87fcfb55cca4b02c68b07dfeb6067c56cbfd
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-21 08:07:31 -0500

    Ejercicio 5

--- js.js
+++ js.js
@@ -1,12 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`24. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le sobran $91000. ¿cuánto dinero tenía?`)
+    console.log(`25. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para hacer el bizcocho es`)
 
-    var juegos=11500*5
-    var bomba=1168000
-    var pernos=87000*3
-    var sobran= 91000
 
-    let suma=(juegos+bomba+pernos+sobran)
+    var harina=100
+    var kakao=10
+    var maskakao=20
 
-    console.log(`El dinero que Tenia en total es ${suma}`)
+    let mult=harina*maskakao
+    let div=mult/kakao
+
+    console.log(`La cantidad de gramos de harina para hacer el bizcocho es: ${div}`)
 })
